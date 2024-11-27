4e887763214badeaa8c118c34d71180f3d30aaf1
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-11-27 11:23:21 +0200

    fix: eslint issuues

--- src/components/shared-theme/customizations/navigation.js
+++ src/components/shared-theme/customizations/navigation.js
@@ -58,6 +58,7 @@ export const navigationCustomizations = {
   },
   MuiSelect: {
     defaultProps: {
+      // eslint-disable-next-line react/display-name
       IconComponent: React.forwardRef((props, ref) => (
         <UnfoldMoreRoundedIcon fontSize="small" {...props} ref={ref} />
       ))
