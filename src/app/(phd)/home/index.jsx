8b5e6c4c2474c00d661b95df300632d76f714641
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-12-01 23:03:10 +0200

    feat: added unauth page logic

--- src/app/(phd)/home/index.jsx
+++ src/app/(phd)/home/index.jsx
@@ -1,55 +1,14 @@
 "use client";
 
-import { callMsGraph } from "@/lib/auth/graph";
-import { setUser } from "@/features/user/userSlice.jsx";
-import { loginRequest } from "@/lib/auth/authConfig";
-import { useEffect } from "react";
+import SignIn from "@/components/auth/SignIn";
 import {
   AuthenticatedTemplate,
-  UnauthenticatedTemplate,
-  useMsal
+  UnauthenticatedTemplate
 } from "@azure/msal-react";
 import "./App.css";
 import Dashboard from "@/components/dashboard/Dashboard";
-import { Box, Button, Container, Paper, Typography } from "@mui/material";
-import { useDispatch } from "react-redux";
-import MicrosoftIcon from "@mui/icons-material/Microsoft";
 
 export default function App() {
-  const { instance, accounts } = useMsal();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    // BUG: Needs 1 second delay to initialize msalInstance
-    const delayTimeout = setTimeout(() => {
-      if (instance && accounts.length > 0) {
-        requestProfileData();
-      }
-    }, 1000);
-    return () => clearTimeout(delayTimeout);
-  }, [instance, accounts]);
-
-  function requestProfileData() {
-    instance
-      .acquireTokenSilent({
-        ...loginRequest,
-        account: accounts[0]
-      })
-      .then((response) => {
-        dispatch(setUser({ response }));
-
-        callMsGraph(response.accessToken).then((response) =>
-          setGraphData(response)
-        );
-      });
-  }
-
-  const handleLogin = () => {
-    instance.loginPopup(loginRequest).catch((e) => {
-      console.log(e);
-    });
-  };
-
   return (
     <>
       <div className="App">
@@ -58,48 +17,7 @@ export default function App() {
         </AuthenticatedTemplate>
 
         <UnauthenticatedTemplate>
-          <Container
-            maxWidth="sm"
-            sx={{
-              display: "flex",
-              minHeight: "100vh",
-              textAlign: "center",
-              paddingTop: 5,
-              alignItems: "center",
-              justifyContent: "justify"
-            }}
-          >
-            <Paper elevation={3} sx={{ padding: 4 }}>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center"
-                }}
-              >
-                <MicrosoftIcon color="lightskyblue" sx={{ fontSize: 60 }} />
-              </Box>
-
-              <Typography variant="h4" color="blue" gutterBottom>
-                401 - Unauthorized
-              </Typography>
-
-              <Typography variant="body1" paragraph>
-                You do not have permission to view this page. Please check your
-                credentials or contact the administrator.
-              </Typography>
-
-              <Box sx={{ marginTop: 2 }}>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handleLogin}
-                  sx={{ marginRight: 2 }}
-                >
-                  Sign in
-                </Button>
-              </Box>
-            </Paper>
-          </Container>
+          <SignIn />
         </UnauthenticatedTemplate>
       </div>
     </>
--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,46 +1,73 @@
+import { Box, Container, Paper, Typography } from "@mui/material";
+import MicrosoftIcon from "@mui/icons-material/Microsoft";
+import AppTheme from "../shared-theme/AppTheme";
+import { LoadingButton } from "@mui/lab";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import AuthHook from "@/hooks/AuthHook";
+import { useState } from "react";
+
 export default function SignIn() {
+  const { handleLogin } = AuthHook();
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
   return (
-    <Container
-      maxWidth="sm"
-      sx={{
-        display: "flex",
-        minHeight: "100vh",
-        textAlign: "center",
-        paddingTop: 5,
-        alignItems: "center",
-        justifyContent: "justify"
-      }}
-    >
-      <Paper elevation={3} sx={{ padding: 4 }}>
-        <Box
+    <AppTheme>
+      <CssBaseline enableColorScheme />
+      <Box
+        sx={(theme) => ({
+          flexGrow: 1,
+          backgroundColor: theme.vars
+            ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
+            : alpha(theme.palette.background.default, 1),
+          overflow: "auto",
+          display: "flex"
+        })}
+      >
+        <Container
+          maxWidth="sm"
           sx={{
             display: "flex",
-            justifyContent: "center"
+            minHeight: "100vh",
+            textAlign: "center",
+            paddingTop: 5,
+            alignItems: "center",
+            justifyContent: "justify"
           }}
         >
-          <MicrosoftIcon color="lightskyblue" sx={{ fontSize: 60 }} />
-        </Box>
+          <Paper elevation={3} sx={{ padding: 4 }}>
+            <Box>
+              <MicrosoftIcon color="lightskyblue" sx={{ fontSize: 60 }} />
+            </Box>
 
-        <Typography variant="h4" color="blue" gutterBottom>
-          401 - Unauthorized
-        </Typography>
+            <Typography variant="h4" color="info" gutterBottom>
+              401 - Unauthorized
+            </Typography>
 
-        <Typography variant="body1" paragraph>
-          You do not have permission to view this page. Please check your
-          credentials or contact the administrator.
-        </Typography>
+            <Typography variant="body1" paragraph>
+              You do not have permission to view this page. Please check your
+              credentials or contact the administrator.
+            </Typography>
 
-        <Box sx={{ marginTop: 2 }}>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleLogin}
-            sx={{ marginRight: 2 }}
-          >
-            Sign in
-          </Button>
-        </Box>
-      </Paper>
-    </Container>
+            <Box sx={{ marginTop: 2 }}>
+              <LoadingButton
+                color="info"
+                disabled={isButtonDisabled}
+                loading={isButtonDisabled}
+                size="medium"
+                variant="contained"
+                onClick={() => {
+                  setIsButtonDisabled(true);
+                  handleLogin();
+                }}
+                sx={{ marginRight: 2 }}
+              >
+                Sign in
+              </LoadingButton>
+            </Box>
+          </Paper>
+        </Container>
+      </Box>
+    </AppTheme>
   );
 }
