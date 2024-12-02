1e9beb79585a171cb8fe57e0b3b47018448a9a32
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-12-02 12:07:19 +0200

    feat: modified page not found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,53 +1,13 @@
 "use client";
 
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import { Button } from "@mui/material";
-import Grid from "@mui/material/Grid2";
+import NotFound from "@/components/error/NotFound";
 
-const NotFound = () => {
+const NotFoundPage = () => {
   return (
     <>
-      <title>404 Not Found</title>
-
-      <Grid
-        container
-        alignItems="center"
-        rowSpacing={22}
-        justifyContent="center"
-        sx={{
-          minHeight: "100vh",
-          textAlign: "center"
-        }}
-      >
-        <Grid xs={12} sm={8} md={6}>
-          <Typography variant="h3">Sorry, page not found!</Typography>
-
-          <Typography sx={{ color: "text.secondary" }}>
-            Sorry, we couldn’t find the page you’re looking for.
-          </Typography>
-
-          <Box
-            component="img"
-            src="/logo.svg"
-            alt="Logo"
-            sx={{
-              width: "50%",
-              maxWidth: 320,
-              height: "auto",
-              margin: "0 auto"
-            }}
-          />
-
-          <Grid xs={12}>
-            <Button href="/" size="large" variant="contained" color="primary">
-              Go to Main Page
-            </Button>
-          </Grid>
-        </Grid>
-      </Grid>
+      <NotFound />
     </>
   );
 };
 
-export default NotFound;
+export default NotFoundPage;
