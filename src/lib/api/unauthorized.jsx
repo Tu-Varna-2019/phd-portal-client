42f0cf4ae62672f82ff3354eeb8f75806482599c
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-02-24 19:52:17 +0200

    refact: doctoralCenter role
    M  src/app/api/unauthorized/login/route.js
    R  src/app/doctoralCenter/(magager-expert)/(home)/page.jsx -> src/app/doctoralCenter/expert/(home)/page.jsx
    R  src/app/doctoralCenter/(magager-expert)/_components/DoctoralCenterCandidatesGrid.jsx -> src/app/doctoralCenter/expert/_components/DoctoralCenterCandidatesGrid.jsx
    R  src/app/doctoralCenter/(magager-expert)/_components/DoctoralCenterExamsGrid.jsx -> src/app/doctoralCenter/expert/_components/DoctoralCenterExamsGrid.jsx
    R  src/app/doctoralCenter/(magager-expert)/_components/DoctoralCenterUnauthorizedUsersGrid.jsx -> src/app/doctoralCenter/expert/_components/DoctoralCenterUnauthorizedUsersGrid.jsx
    R  src/app/doctoralCenter/(magager-expert)/_components/DoctralCenterHomeGrid.jsx -> src/app/doctoralCenter/expert/_components/DoctralCenterHomeGrid.jsx
    R  src/app/doctoralCenter/(magager-expert)/_lib/UnauthorizedUsersGridData.jsx -> src/app/doctoralCenter/expert/_lib/UnauthorizedUsersGridData.jsx
    R  src/app/doctoralCenter/(magager-expert)/candidates/page.jsx -> src/app/doctoralCenter/expert/candidates/page.jsx
    R  src/app/doctoralCenter/(magager-expert)/exams/page.jsx -> src/app/doctoralCenter/expert/exams/page.jsx
    R  src/app/doctoralCenter/(magager-expert)/notifications/page.jsx -> src/app/doctoralCenter/expert/notifications/page.jsx
    R  src/app/doctoralCenter/(magager-expert)/profile/page.jsx -> src/app/doctoralCenter/expert/profile/page.jsx
    R  src/app/doctoralCenter/(magager-expert)/unauthorized-users/page.jsx -> src/app/doctoralCenter/expert/unauthorized-users/page.jsx
    M  src/app/page.jsx
    M  src/hooks/AuthHook.jsx
    M  src/lib/api/unauthorized.jsx
    M  src/middleware.js

--- src/lib/api/unauthorized.jsx
+++ src/lib/api/unauthorized.jsx
@@ -7,16 +7,15 @@ const API_URL = "/api/unauthorized/login";
 export default function UnauthorizedAPI() {
   const { route } = ClientRoute();
 
-  const fetchLogin = async (userCreds, accessToken) => {
+  const login = async (accessToken) => {
     return await route({
       url: API_URL,
       method: "POST",
-      body: userCreds,
       accessTokenProvided: accessToken
     });
   };
 
   return {
-    fetchLogin
+    login
   };
 }
