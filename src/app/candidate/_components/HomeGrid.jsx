d8989db490ac514b9043ab1e86c020f385f0cee1
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-03-07 20:28:27 +0200

    chore: refactored home component for candidate
    M  src/app/candidate/_components/HomeGrid.jsx

--- src/app/candidate/_components/HomeGrid.jsx
+++ src/app/candidate/_components/HomeGrid.jsx
@@ -4,7 +4,7 @@ import HomeHook from "../_hooks/HomeHook";
 import { columns, subjectColumns } from "../_constants/columnsConstant";
 import Table from "@/components/main-layout/common/Table";
 import { useTranslation } from "react-i18next";
-import { Divider } from "@mui/material";
+import { Card, CardActionArea, Divider } from "@mui/material";
 
 export default function HomeGrid() {
   const { curriculums, curriculumSubjects } = HomeHook();
@@ -12,35 +12,47 @@ export default function HomeGrid() {
   let idGridKey = 0;
 
   return (
-    <Grid container spacing={4} columns={12}>
-      <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
-        Добре дошли в докторантурската програма на Технически университет Варна
-      </Typography>
-
-      <Grid size={{ xs: 12, lg: 9 }}>
-        <Typography component="h3" variant="h7" sx={{ mb: 2 }} align="center">
-          {t(
-            "Curriculum of the Doctoral Programme for the Degree of Doctor of Education and Science"
-          )}
+    <Grid container spacing={8} columns={12}>
+      <>
+        <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
+          Добре дошли в докторантурската програма на Технически университет
+          Варна
         </Typography>
-        <Divider />
-        <Table rows={curriculums} columns={columns} />
-      </Grid>
 
+        <Grid size={{ xs: 12, lg: 9 }}>
+          <Typography component="h3" variant="h7" sx={{ mb: 2 }} align="center">
+            {t(
+              "Curriculum of the Doctoral Programme for the Degree of Doctor of Education and Science"
+            )}
+          </Typography>
+          <Divider />
+          <Table rows={curriculums} columns={columns} />
+        </Grid>
+      </>
+      <Typography component="h3" variant="h7" align="center">
+        {t(
+          "Curriculum of the Doctoral Programme for the Degree of Doctor of Education and Science"
+        )}
+      </Typography>
+      <Divider />
       {curriculumSubjects.map((curriculum) => {
         return (
-          <Grid size={{ xs: 12, lg: 9 }} key={idGridKey++}>
-            <Typography
-              component="h3"
-              variant="h7"
-              sx={{ mb: 2 }}
-              align="center"
-            >
-              {t(curriculum.description)}
-            </Typography>
-            <Divider />
-            <Table rows={curriculum.subjects} columns={subjectColumns} />
-          </Grid>
+          <Card key={idGridKey}>
+            <Grid key={idGridKey++}>
+              <CardActionArea>
+                <Typography
+                  component="h3"
+                  variant="h7"
+                  sx={{ mb: 2 }}
+                  align="center"
+                >
+                  {t(curriculum.description)}
+                </Typography>
+                <Divider />
+                <Table rows={curriculum.subjects} columns={subjectColumns} />
+              </CardActionArea>
+            </Grid>
+          </Card>
         );
       })}
     </Grid>
