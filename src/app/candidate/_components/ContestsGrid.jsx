7368ca93ddf16fd0f7115510a77f0243e3f2af44
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-03-10 17:05:49 +0200

    feat: finalized contests page
    M  src/app/candidate/_components/ContestsGrid.jsx
    M  src/app/candidate/_hooks/ContestsHook.jsx

--- src/app/candidate/_components/ContestsGrid.jsx
+++ src/app/candidate/_components/ContestsGrid.jsx
@@ -7,43 +7,37 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
-import { TabContext, TabPanel } from "@mui/lab";
 import ContestsHook from "../_hooks/ContestsHook";
 
 export default function ContestsGrid() {
-  const { contestYears, contestCandidatesByYear } = ContestsHook();
-  const [value, setValue] = React.useState(0);
+  const { contestYears, selectedCandidatesByYear, setSelectedYearContest } =
+    ContestsHook();
+  const [tabState, setTabState] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
+  const handleChange = (_, tabIndexValue) => {
+    setTabState(tabIndexValue);
+    setSelectedYearContest(contestYears[tabIndexValue]);
   };
   const { t } = useTranslation("client-page");
 
   return (
-    <Grid container spacing={4} columns={13}>
+    <Grid container spacing={4} columns={5}>
       <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
         {t("Announcement of competitions for PhD students")}
       </Typography>
 
       <Grid size={{ xs: 12, lg: 9 }}>
         <Box sx={{ width: "100%" }}>
-          {contestYears.map((year, index) => {
-            return (
-              <Tabs value={value} onChange={handleChange} centered>
-                <Tab label={year} />
-              </Tabs>
-            );
-          })}
-
-          <TabContext value={0}>
-            <TabPanel value={value}>
-              <Table
-                rows={contestCandidatesByYear}
-                columns={contestsColumns}
-                density="comfortable"
-              />
-            </TabPanel>
-          </TabContext>
+          <Tabs value={tabState} onChange={handleChange} centered>
+            {contestYears.map((year, index) => {
+              return <Tab key={index} label={year} />;
+            })}
+          </Tabs>
+          <Table
+            rows={selectedCandidatesByYear}
+            columns={contestsColumns}
+            density="comfortable"
+          />
         </Box>
       </Grid>
     </Grid>
--- src/app/candidate/_hooks/ContestsHook.jsx
+++ src/app/candidate/_hooks/ContestsHook.jsx
@@ -1,15 +1,18 @@
 import CandidateAPI from "@/lib/api/candidate";
 import { useCallback, useEffect, useMemo, useState } from "react";
+import { useTranslation } from "react-i18next";
 
 export default function ContestsHook() {
   const [contests, setContests] = useState([]);
   const [selectedContestYear, setSelectedYearContest] = useState();
   const { getContests } = CandidateAPI();
+  const { t, ready } = useTranslation("client-page");
 
   const fetchContests = useCallback(async () => {
     const contestsRes = await getContests();
     contestsRes.forEach((contest, index) => {
       contest.id = index;
+      contest.faculty = ready ? t(contest.faculty) : contest.faculty;
       return contest;
     });
     contestsRes.sort(
@@ -29,7 +32,7 @@ export default function ContestsHook() {
     return Array.from(new Set(contests.map((contest) => contest.yearAccepted)));
   }, [contests]);
 
-  const contestCandidatesByYear = useMemo(() => {
+  const selectedCandidatesByYear = useMemo(() => {
     return contests.filter(
       (contest) => contest.yearAccepted == selectedContestYear
     );
@@ -37,6 +40,7 @@ export default function ContestsHook() {
 
   return {
     contestYears,
-    contestCandidatesByYear
+    selectedCandidatesByYear,
+    setSelectedYearContest
   };
 }
