d20ec4e72685efe06a64d03dea7d447e6423f4e7
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-07-05 18:13:47 +0200

    fix: stupid NextJS
    M  src/app/api/committee/chairman-member/grade/evaluate/[type]/route.js
    M  src/app/api/doctoral-center/manager-expert/candidate/[email]/application/[status]/route.js
    M  src/app/api/doctoral-center/manager-expert/grade/[id]/commission/[name]/route.js

--- src/app/api/doctoral-center/manager-expert/candidate/[email]/application/[status]/route.js
+++ src/app/api/doctoral-center/manager-expert/candidate/[email]/application/[status]/route.js
@@ -4,7 +4,7 @@ import { url } from "../../../../url";
 const { route } = ServerRoute();
 
 export async function PATCH(request, { params }) {
-  const { email, _, status } = params;
+  const { email, _, status } = await params;
 
   return await route({
     url: url + `/candidate/${email}/application/${status}`,
